2c68e60742fe160dd26fb9800a63ceb141217aad
eletim <rext7th@gmail.com>
2026-09-15 20:32:25 +0900

    Update sample listen defaults

--- sample_config.sh
+++ sample_config.sh
@@ -3,5 +3,5 @@
 # Copy this file to config.sh or run ./start.sh --configure. Values already set
 # in the environment take precedence over these local defaults.
 
-export HOST="${HOST:-127.0.0.1}"
-export PORT="${PORT:-3000}"
+export HOST="${HOST:-0.0.0.0}"
+export PORT="${PORT:-8023}"
